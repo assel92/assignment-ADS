bbe2074af9465264495638175514ec68d15e3d78
assel <asselrahmet791@gmail.com>
2026-03-27 12:15:16 +0500

    assignment 1 ADS

--- src/task_2.java
+++ src/task_2.java
@@ -1,19 +1,23 @@
-public static int sum (int a,  int[] n){
+public static int sum (int a,  ArrayList<Integer> n){
     if (a<1){
         return(0);
     }
     else{
         a= a-1;
-        return (n[a]+sum(a,n) );
+        return (n.get(a) +sum(a,n) );
     }
+}
+public static ArrayList<Integer> input (Scanner sc, int a, ArrayList<Integer> n){
+    if (a==0){
+        return n;}
+    n.add(sc.nextInt());
+    return input(sc, a-1, n);
+
 }
 public static void main (String[] args)
 { Scanner sc = new Scanner(System.in);
     int a = sc.nextInt();
-    int[] n = new int[a];
-    for (int i = 0; i < a; i++) {
-        n[i] = sc.nextInt();
-    }
+    ArrayList<Integer> n = input(sc,a,new ArrayList<>());
     double average = (double) sum(a, n) /a;
     System.out.println(average);
 }
