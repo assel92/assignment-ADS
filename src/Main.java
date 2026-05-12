e21f363906d067ee72e135349847550189d80e14
assel <asselrahmet791@gmail.com>
2026-05-12 11:42:18 +0500

    ADS Assignment 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Experiment experiment = new Experiment();
 
         experiment.runMultipleTests();
